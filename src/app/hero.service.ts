39d80085104fb5e4875dd27498e7c98288da2bde
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-03 13:32:35 +0100

    Setting the service
    The service previously created now pass the data to the hero component instead of this last one fetching them directly. And could work with asynchronous request if we were fetching data from a distant server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,13 @@
+//This service exist because component shouldn't fetch or save data themselves nor knowingly show fake data
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+/*
+  We import Observable and of to simulate the fact that we fetch data from a distant server.
+  Therefore our previous code was working because we run the app as is and don't make any
+  asynchronous action which is normally not the case for real app.
+*/
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +15,10 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  //Now we return an Observable array that emit a single value(the heroes list)
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; //imported to know what a hero is in this component
-import { HEROES } from '../mock-heroes'; //imported to know the list of hero in this component
+import { HeroService } from '../hero.service'; //The service is what should provide Heroes data
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +10,26 @@ import { HEROES } from '../mock-heroes'; //imported to know the list of hero in 
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES; //pass the heroes list in the variable used in the HTML of the component
+  heroes: Hero[] = []; //This property will be filled by the service
   selectedHero?: Hero; //Check if a Hero object is selected when clicking on the list
 
-  constructor() { }
+  constructor(private heroService: HeroService) {} //Initialize the service when the component is constructed
 
   ngOnInit() {
+    this.getHeroes(); //Fill the heroes property when the component is intialized
   }
 
   //When a hero is selected the form is displayed by setting the variable selectedHero
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  /*
+    Now that we have subscribed to the method, the asynchronous request should work
+    if we fetch data from a distant server.
+  */
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
