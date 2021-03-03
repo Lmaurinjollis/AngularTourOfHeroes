a53bf453be220fbef223b02ef2ffb3596772a7dc
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-03 12:52:52 +0100

    Separating details
    The form and details of the selected hero are now in a new component named hero-detail.
    The component was generated by the CLI:
    ng generate component hero-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; //Must be included for the hero's form
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component'; //Imported by the CLI's command when generating the component
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; //Imported by the CLI's command when generating the component
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent //Declared automatically by the CLI's command when generating the component
+    HeroesComponent,
+    HeroDetailComponent //Declared automatically by the CLI's command when generating the component
   ],
   imports: [
     BrowserModule,
