459d829860686c0fa532cdf63f97db073322a0ec
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-04 10:44:43 +0100

    Dashboard
    Adding the dashboard component and set its route.
    Default route redirect to this component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  //Default route will redirect to the dashboard
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  //This route will redirect to the hero list
+  { path: 'heroes', component: HeroesComponent },
+  //This route will redirect to the hero dashboard
+  { path: 'dashboard', component: DashboardComponent }
+
 ];
 
 @NgModule({
