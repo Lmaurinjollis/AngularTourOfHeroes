bae1ddc8d0c32fb003b69fee84ed2d95c72f01cc
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-04 11:29:13 +0100

    Mock server
    Using the CLI's command:
    npm install angular-in-memory-web-api --save
    to generate a mock server and linking it to the app with a new service InMemoryData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; //Must be included for the hero's form
+import { HttpClientModule } from '@angular/common/http'; //This module will allow fetching data on a server
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 /*Imported by the CLI's command when generating the component/module */
@@ -21,7 +24,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,//It must be declared in the import to use it
+    /* The two following module must be added to be used */
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false}),
+    FormsModule,
     /*Declared automatically by the CLI's command after the import*/
     AppRoutingModule
   ],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 /*
   We import Observable and of to simulate the fact that we fetch data from a distant server.
   Therefore our previous code was working because we run the app as is and don't make any
@@ -16,19 +16,25 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  //Now we return an Observable array that emit a single value(the heroes list)
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    //We also send a message with the message service when heroes are fetched
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    //return an Observable array that emit a single value(the heroes list)
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
